a80684c66db3c3e8f4c649769dd66961ab46b016
2dam3 <2dam3@HZ114485.H012108.NET>
2022-10-07 10:00:50 +0200

    obteniendo datos

--- src/clases/Personas.java
+++ src/clases/Personas.java
@@ -7,8 +7,7 @@ import java.nio.file.Path;
 public class Personas {
     private String dni, nombre;
     private int edad;
-
-    String ruta = "src/info/datos.txt";
+    private Path path = Path.of("src/info/datos.txt");
 
     public Personas(String nombre, String dni, int edad) {
         this.nombre = nombre;
@@ -21,22 +20,26 @@ public class Personas {
 
     public void insertarDatos(){
         try{
-            RandomAccessFile accederFichero  = new RandomAccessFile(new File(ruta), "rws");
-
-            accederFichero.write(getNombre().getBytes(),0,22);
-            accederFichero.write(getEdad().getBytes(),22,24);
-            accederFichero.write(getDni().getBytes(),24,32);
-
-
+            RandomAccessFile accederFichero  = new RandomAccessFile(new File(path.toUri()), "rws");
+//            Nombre: 22 bytes      Edad: 2 bytes       Dni: 8 bytes
+            accederFichero.seek(accederFichero.length());
+            accederFichero.write(String.format("%1$-" + 22 + "s", (getNombre())).getBytes(),0,22);
+            accederFichero.write(" ".getBytes());
+            accederFichero.write(String.format("%1$-" + 3 + "s", (getEdad())).getBytes(),0,3);
+            accederFichero.write(" ".getBytes());
+            accederFichero.write(String.format("%1$-" + 9 + "s", (getDni())).getBytes(),0,9);
+            accederFichero.write(String.format("\n").getBytes(), 0,1);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+    }
 
+    public void buscarPersona(String dniBusqueda){
 
     }
 
-    public void leerDatos(){
-
+    public void imprimirDatosInsertados(){
+        System.out.println("Nombre: " + nombre + " Dni: " + dni + " Edad: " + edad);
     }
 
     public String getDni() {
--- src/main.java
+++ src/main.java
@@ -1,15 +1,70 @@
 
+import clases.Personas;
+
 import java.util.Scanner;
 
 public class main {
     static Scanner in = new Scanner(System.in);
+    private static Personas personas = new Personas();
+    private static String nombre, dni;
+    private static Integer edad;
     public static void main(String[] args){
-        int opcion = 1;
+        int opcion = 3;
+        mostrarInfo();
         while (opcion != 0) {
+            opcion = leerOpcion(3);
             switch(opcion) {
-
+                case 0:
+                    System.out.println("Hasta la proxima");
+                    System.exit(0);
+                case 1:
+//                    TODO: Arreglar esta zona ----------------
+                    System.out.println("Introduce el dni a buscar");
+                    String dniBusqueda = in.nextLine();
+                    personas.buscarPersona(dniBusqueda);
+                    System.out.println("***************** Dejando de leer *****************");
+                    opcion = 3;
+                case 2:
+                    System.out.println("Nombre: ");
+                    nombre = in.nextLine();
+                    System.out.println("Dni: ");
+                    dni = in.nextLine();
+                    System.out.println("Edad: ");
+                    edad = in.nextInt();
+                    Personas personas = new Personas(nombre,dni,edad);
+                    personas.insertarDatos();
+                    System.out.println("***************** Datos insertados *****************");
+                    opcion = 3;
+                case 3:
+                    mostrarInfo();
+                    break;
             }
         }
+    }
+
+    private static int leerOpcion(int max) {
+        boolean terminar = false;
+        int n = 0;
+        while (!terminar) {
+            try {
+                n = in.nextInt();
+                in.nextLine();
+                if (n>=max || n<0) {
+                    throw new Exception();
+                }
+                terminar = true;
+            } catch (Exception e) {
+                System.out.println("Opción incorrecta! elije de nuevo");
+            }
+        }
+        return n;
+    }
+
+    private static void mostrarInfo() {
+        System.out.println("Elige una de estas opciones");
+        System.out.println("1 - Buscar persona");
+        System.out.println("2 - Insertar datos");
+        System.out.println("0 - Salir");
     }
 
 
